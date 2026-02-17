85fc7a83ea7038d234cd8d855cd4cfdaccee91d3
CadenAnderson04 <156140513+CadenAnderson04@users.noreply.github.com>
2026-02-17 10:37:05 -0700

    Add files via upload

---
+++ circuit_b.v
@@ -0,0 +1,14 @@
+module circuit_b(
+    // Declare inputs\
+    input A,B,C,D,
+          
+    // Declare Y output
+    output Y
+);
+
+    // Enter logic equation here
+   assign  Y = A & B |
+                ~C & ~D|
+                B & ~D;
+
+endmodule
